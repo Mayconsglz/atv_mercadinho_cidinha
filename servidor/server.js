7cee83d13fb96d146f428435de73ad5054adcd44
Mayconsgl <maycon.senai2a@gmail.com>
2026-09-17 16:18:04 -0300

    Update server.js

--- servidor/server.js
+++ servidor/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const compras = require("../dados.json")
 
 const mostrarCompras = (req, res) => {
+    calcularSubtotais()
     res.send(compras)
 }
 
@@ -9,9 +10,15 @@ const novaCompra = (req, res) => {
     if(req.body){
         res.send("Compra recebida, e em separação")
         compras.push(req.body)
-    }else{
+    }else{ 
         res.send("Ocorreu um erro ao receber o pedido de compra")
     }
+}
+
+const calcularSubtotais = () => {
+    pedidos.forEach(p=>{
+        p.subtotal = p.precoUnitario * p.quantidade
+    })
 }
 
 const app = express()
